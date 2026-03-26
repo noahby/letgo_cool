34b07acb4363dc370aa5d6d954f5c81ac3338bc0
noahby <noah.beyer@permakulturhof.de>
2026-03-26 18:07:06 +0100

    Update constants.py

--- strategies/constants.py
+++ strategies/constants.py
@@ -3,15 +3,18 @@
 # ********************************** SPY TIPS COOL STRATEGY **********************************
 SPY_SMA = 150
 TIPS_SMA = 200
+GOLD_SMA = 200
+
 COOLDOWN_DAYS = 15
 
 # number of attempts to download data
 TRY_COUNT = 3
-DAILY_NOTIFICATION = True # Write True to send a daily notification, False to only send notifications when the signal changes
+DAILY_NOTIFICATION = True
 
 # main subjects
 MAIN_SIGNAL_CHANGE_LONG = f"GO LONG NOW (cooldown activated for {0} days)"
 MAIN_SIGNAL_CHANGE_SHORT = f"GO IN CASH NOW (cooldown activated for {0} days)"
+MAIN_SIGNAL_CHANGE_GOLD = f"GO IN GOLD NOW (cooldown activated for {0} days)"
 COOLDOWN_WARNINGS = [1]
 COOLDOWN_WARNINGS_TEXT = ["Cooldown warning: Last day of cooldown"]
 
@@ -21,12 +24,8 @@ assert len(COOLDOWN_WARNINGS) == len(COOLDOWN_WARNINGS_TEXT), "COOLDOWN_WARNINGS
 
 # ********************************** END SPY TIPS COOL STRATEGY **********************************
 
-
-
-
-
-
 # DO NOT CHANGE THESE
 BUY = "BUY"
 SELL = "SELL"
+GOLD = "GOLD"
 HISTORY_FILENAME = "history"
