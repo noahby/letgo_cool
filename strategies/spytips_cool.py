33c651a6895966034b6742a9804022e23fd1b3c7
noahby <noah.beyer@permakulturhof.de>
2026-05-19 18:45:20 +0200

    Update spytips_cool.py

--- strategies/spytips_cool.py
+++ strategies/spytips_cool.py
@@ -70,8 +70,6 @@ def spy_tips_cool():
     tips_diff = (tips_close - tips_sma_rolling) / tips_sma_rolling
     gold_diff = (gold_close - gold_sma_rolling) / gold_sma_rolling
 
-    
-
     # Determine the market state for a given index position:
     # BUY (SPY) if both SPY and TIPS are above SMA,
     # GOLD if only gold is above SMA,
@@ -83,10 +81,6 @@ def spy_tips_cool():
             return GOLD
         else:
             return SELL
-    print("TIP date=", tips.index[-1])
-    print("TIP close=", tips_close.iloc[-1])
-    print("TIP sma=", tips_sma_rolling.iloc[-1])
-    print("TIP pct=", tips_diff.iloc[-1])
 
     fileName = HISTORY_FILENAME + "_" + str(SPY_SMA) + "_" + str(TIPS_SMA) + "_" + str(COOLDOWN_DAYS) + ".txt"
     last_entry = None
