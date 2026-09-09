4a1c90547f69d4a8ef85d4ab411e7bcf0e475bda
noahby <noah.beyer@permakulturhof.de>
2026-09-09 14:33:06 +0200

    Refactor spy_tips_cool to exclude gold data
    Removed gold data processing and adjusted calculations to focus on SPY and TIPS only.

--- strategies/spytips_cool.py
+++ strategies/spytips_cool.py
@@ -16,34 +16,36 @@ def calculate_sma(data, n):
     return sum(data[-n:]) / n
 
 def spy_tips_cool():
+    # NEU: Nur noch SPY und TIPS, kein Gold mehr
     spy_data = fetch_yahoo_data("^SP500TR")
     tips_data = fetch_yahoo_data("TIP")
-    gold_data = fetch_yahoo_data("GC=F")
+    # gold_data = fetch_yahoo_data("GC=F")  # ENTFERNT
 
-    if not spy_data or not tips_data or not gold_data:
-        return None, None, None, None, 0, 0, 0
+    if not spy_data or not tips_data:  # Kein Gold mehr benötigt
+        return None, None, None, 0, 0
 
     spy_close  = spy_data[-1]
     tips_close = tips_data[-1]
-    gold_close = gold_data[-1]
+    # gold_close = gold_data[-1]  # ENTFERNT
 
-    spy_sma  = calculate_sma(spy_data, 150)
-    tips_sma = calculate_sma(tips_data, 200)
-    gold_sma = calculate_sma(gold_data, 175)
+    # NEU: SMA160/160 statt 150/200
+    spy_sma  = calculate_sma(spy_data, 160)
+    tips_sma = calculate_sma(tips_data, 160)
+    # gold_sma = calculate_sma(gold_data, 175)  # ENTFERNT
 
     spy_ok  = spy_close > spy_sma
     tips_ok = tips_close > tips_sma
-    gold_ok = gold_close > gold_sma
+    # gold_ok = gold_close > gold_sma  # ENTFERNT
 
+    # NEU: Nur noch binäres Signal (Buy/Cash), kein Gold mehr
     if spy_ok and tips_ok:
         current_signal = "Buy"
-    elif gold_ok:
-        current_signal = "Gold"
     else:
         current_signal = "Cash"
 
     spy_diff  = ((spy_close  - spy_sma)  / spy_sma)  * 100
     tips_diff = ((tips_close - tips_sma) / tips_sma) * 100
-    gold_diff = ((gold_close - gold_sma) / gold_sma) * 100
+    # gold_diff = ((gold_close - gold_sma) / gold_sma) * 100  # ENTFERNT
 
-    return current_signal, spy_ok, tips_ok, gold_ok, spy_diff, tips_diff, gold_diff
+    # NEU: Nur 5 Return-Werte statt 7 (kein Gold mehr)
+    return current_signal, spy_ok, tips_ok, spy_diff, tips_diff
